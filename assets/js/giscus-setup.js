5e4f9964bc97c035dcfd5e4f3864d9f3df87abef
dsimon45 <81932137+dsimon45@users.noreply.github.com>
2026-05-18 19:27:20 +0000

    Deploying to gh-pages from @ dsimon45/dsimon45.github.io@542dbc25daeaa06ee6b9b5f77051138856faeae3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "dsimon45/das_website",
+    "data-repo": "dsimon45/dsimon45.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
